b9cfcda4c6ee29a3da897cd50d7178d4bbf673c6
padmaja0793 <padmaja7210@gmail.com>
2025-08-20 01:43:57 +0100

    js codes

---
+++ getOwnPro.js
@@ -0,0 +1,14 @@
+//OBJECT
+//getOwnPropertyName   
+
+let obj =  profile;
+{
+    names : "Padmaja";
+    email : "padmaja7210@gmail.com";
+    age : 22;
+}
+console.log(object.getOwnProperty(profile));
+
+
+
+
---
+++ replace.js
@@ -0,0 +1,39 @@
+
+//REPLACE  =  Its going the replace first given the word specify replace the word.  
+//string.replace(searchValue, newValue)
+
+//let Str = "WelcometoUSSWelcometoUSSWelcometoUSS";
+//console.log(Str.replace("USS","******"));
+
+
+
+
+
+
+
+
+//replace all
+
+//let str = "Padmaja Padmaja Padmaja Padmaja";
+//let result = str.replace(/Padmaja/g, "Pujari");
+//console.log(result);
+
+
+
+
+
+
+
+
+//Case-insensitive replace
+
+let str = "USS USS USS";
+let result = str.replace(/USS/gi, "Class");
+console.log(result);
+
+
+
+
+
+
+
