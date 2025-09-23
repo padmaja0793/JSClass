08196eb7e7a7f287a0620d8f920f3f7883ef4b49
padmaja0793 <padmaja7210@gmail.com>
2025-09-23 11:51:42 +0100

    JS Codes

---
+++ String/S1.js
@@ -0,0 +1,13 @@
+
+let str="Welcome"; //String literal
+
+let str2=new String("Welcome"); //String Object
+
+let str3=String("Welcome"); //String Literal
+
+let str4="welcome"; //template literal
+
+console.log(str);
+console.log(str2);
+console.log(str3);
+console.log(str4);
---
+++ String/S2.js
@@ -0,0 +1,16 @@
+let names="PadmajaLaxmanShivappanor";
+//String starts from zero.
+console.log(names.at(-2));
+//from the end.
+console.log(names.at(2));
+//from the start.
+console.log(names.at());
+//zeroth element.
+
+console.log(typeof(names.at(2)));
+
+console.log("abcdefghij");
+
+console.log(names.charAt(0));
+console.log(names.charAt(-1));
+console.log((names.charAt(100)));
