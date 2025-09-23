46960018dda53906f0345e4214f336437371222b
padmaja0793 <padmaja7210@gmail.com>
2025-09-23 11:55:02 +0100

    JS Codes

---
+++ String/S4.js
@@ -0,0 +1,16 @@
+
+let str="LEARNJS";
+
+console.log(str.slice(1,3));
+console.log(str.slice(1,4));
+//slice :=> start,end=> end excluded.
+
+console.log(str.slice(1,-2));
+//-2 elements from the end.
+
+console.log(str.slice(3));
+//will go upto end from the Index 3.
+console.log(str.slice());
+
+console.log(str.slice(-4,-2));
+console.log(str.slice(-8,2));
